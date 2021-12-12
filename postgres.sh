51a1fa859d77bb6412bba1dd40623d04911ec92e
Timur Batyrshin <erthad@gmail.com>
2021-12-12 20:14:26 +0300

    added installation script

---
+++ postgres.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Install PostgreSQL
+curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+echo 'deb http://apt.postgresql.org/pub/repos/apt/ focal-pgdg main' | sudo tee /etc/apt/sources.list.d/postgres.list
+sudo apt-get update
+sudo apt-get install postgresql-14
+
+# Install ZomboDB
+# see https://github.com/zombodb/zombodb/blob/master/SOURCE-INSTALLATION.md#building-binary-artifacts-with-docker for building
+# see https://github.com/zombodb/zombodb/blob/master/BINARY-INSTALLATION.md for configuration
+sudo dpkg -i zombodb_ubuntu-bionic_pg14-3000.0.6_amd64.deb
+
+# Install ElasticSearch
+curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
+apt-get update && apt-get install elasticsearch
+systemctl start elasticsearch
